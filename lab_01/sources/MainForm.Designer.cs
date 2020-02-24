68da02803e5a07a03672d9dc651d38bd1deb3db0
Pavel Khetagurov <khetagurow1@yandex.ru>
2020-02-24 13:31:05 +0300

    [lab_01] Update GUI

--- lab_01/sources/MainForm.Designer.cs
+++ lab_01/sources/MainForm.Designer.cs
@@ -38,9 +38,21 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -51,7 +63,7 @@
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Info;
             this.pictureBox1.Location = new System.Drawing.Point(189, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(604, 429);
+            this.pictureBox1.Size = new System.Drawing.Size(604, 468);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -145,26 +157,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 447);
+            this.button1.Location = new System.Drawing.Point(12, 444);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(171, 36);
             this.button1.TabIndex = 5;
-            this.button1.Text = "Очистить";
+            this.button1.Text = "Очистить все";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(799, 447);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(171, 36);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "Очистить";
-            this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.LightCoral;
-            this.button3.Location = new System.Drawing.Point(189, 447);
+            this.button3.Location = new System.Drawing.Point(189, 486);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(195, 54);
             this.button3.TabIndex = 7;
@@ -175,19 +178,140 @@
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Info;
-            this.textBox1.Location = new System.Drawing.Point(400, 451);
+            this.textBox1.Location = new System.Drawing.Point(390, 486);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(393, 50);
+            this.textBox1.Size = new System.Drawing.Size(403, 50);
             this.textBox1.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(32, 567);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 29);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Ввести точку";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 539);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(58, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(121, 539);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(62, 22);
+            this.textBox3.TabIndex = 11;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 486);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(171, 36);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Удалить точку";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 542);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "X:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(96, 542);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Y:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(883, 542);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 17);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Y:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(796, 542);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 17);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "X:";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(799, 486);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(171, 36);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Удалить точку";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(908, 539);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(62, 22);
+            this.textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(819, 539);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(58, 22);
+            this.textBox5.TabIndex = 17;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(819, 567);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(137, 29);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Ввести точку";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(799, 444);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(171, 36);
+            this.button8.TabIndex = 15;
+            this.button8.Text = "Очистить все";
+            this.button8.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
-            this.ClientSize = new System.Drawing.Size(982, 516);
+            this.ClientSize = new System.Drawing.Size(982, 606);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -215,8 +339,20 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn X;
         private System.Windows.Forms.DataGridViewTextBoxColumn Y;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
