9bd2655eb505e5d046687cd6ae3dd9e7a544ea13
Pavel Khetagurov <khetagurow1@yandex.ru>
2020-04-20 17:32:54 +0300

    [lab_05] Add lab_05

---
+++ lab_05/sources/MainForm.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace lab_05
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainCanvas = new System.Windows.Forms.PictureBox();
+            this.coordsLabel = new System.Windows.Forms.Label();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.groupColor = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButtonWayQuick = new System.Windows.Forms.RadioButton();
+            this.radioButtonWayStep = new System.Windows.Forms.RadioButton();
+            this.groupBoxWay = new System.Windows.Forms.GroupBox();
+            this.buttonMain = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonHorizontal = new System.Windows.Forms.Button();
+            this.buttonVertical = new System.Windows.Forms.Button();
+            this.groupBoxSpecific = new System.Windows.Forms.GroupBox();
+            this.buttonLastDelete = new System.Windows.Forms.Button();
+            this.resultMessage = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.radioButtonStringest = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.mainCanvas)).BeginInit();
+            this.groupColor.SuspendLayout();
+            this.groupBoxWay.SuspendLayout();
+            this.groupBoxSpecific.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainCanvas
+            // 
+            this.mainCanvas.BackColor = System.Drawing.Color.White;
+            this.mainCanvas.Location = new System.Drawing.Point(11, 6);
+            this.mainCanvas.Margin = new System.Windows.Forms.Padding(2);
+            this.mainCanvas.Name = "mainCanvas";
+            this.mainCanvas.Size = new System.Drawing.Size(615, 615);
+            this.mainCanvas.TabIndex = 0;
+            this.mainCanvas.TabStop = false;
+            this.mainCanvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mainCanvas_MouseClick);
+            this.mainCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainCanvas_MouseMove);
+            // 
+            // coordsLabel
+            // 
+            this.coordsLabel.AutoSize = true;
+            this.coordsLabel.Location = new System.Drawing.Point(11, 623);
+            this.coordsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.coordsLabel.Name = "coordsLabel";
+            this.coordsLabel.Size = new System.Drawing.Size(31, 13);
+            this.coordsLabel.TabIndex = 7;
+            this.coordsLabel.Text = "(0, 0)";
+            // 
+            // colorButton
+            // 
+            this.colorButton.BackColor = System.Drawing.Color.Black;
+            this.colorButton.Location = new System.Drawing.Point(111, 20);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(63, 27);
+            this.colorButton.TabIndex = 35;
+            this.colorButton.UseVisualStyleBackColor = false;
+            this.colorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(631, 12);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(140, 21);
+            this.clearButton.TabIndex = 46;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // groupColor
+            // 
+            this.groupColor.Controls.Add(this.label6);
+            this.groupColor.Controls.Add(this.colorButton);
+            this.groupColor.Location = new System.Drawing.Point(631, 70);
+            this.groupColor.Name = "groupColor";
+            this.groupColor.Size = new System.Drawing.Size(185, 59);
+            this.groupColor.TabIndex = 53;
+            this.groupColor.TabStop = false;
+            this.groupColor.Text = "Выбор цвета";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "Выбранный цвет: ";
+            // 
+            // radioButtonWayQuick
+            // 
+            this.radioButtonWayQuick.AutoSize = true;
+            this.radioButtonWayQuick.Checked = true;
+            this.radioButtonWayQuick.Location = new System.Drawing.Point(10, 19);
+            this.radioButtonWayQuick.Name = "radioButtonWayQuick";
+            this.radioButtonWayQuick.Size = new System.Drawing.Size(97, 17);
+            this.radioButtonWayQuick.TabIndex = 54;
+            this.radioButtonWayQuick.TabStop = true;
+            this.radioButtonWayQuick.Text = "Без задержки";
+            this.radioButtonWayQuick.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonWayStep
+            // 
+            this.radioButtonWayStep.AutoSize = true;
+            this.radioButtonWayStep.Location = new System.Drawing.Point(10, 42);
+            this.radioButtonWayStep.Name = "radioButtonWayStep";
+            this.radioButtonWayStep.Size = new System.Drawing.Size(91, 17);
+            this.radioButtonWayStep.TabIndex = 55;
+            this.radioButtonWayStep.Text = "С задержкой";
+            this.radioButtonWayStep.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxWay
+            // 
+            this.groupBoxWay.Controls.Add(this.radioButtonStringest);
+            this.groupBoxWay.Controls.Add(this.radioButtonWayQuick);
+            this.groupBoxWay.Controls.Add(this.radioButtonWayStep);
+            this.groupBoxWay.Location = new System.Drawing.Point(631, 135);
+            this.groupBoxWay.Name = "groupBoxWay";
+            this.groupBoxWay.Size = new System.Drawing.Size(185, 86);
+            this.groupBoxWay.TabIndex = 56;
+            this.groupBoxWay.TabStop = false;
+            this.groupBoxWay.Text = "Способ вывода";
+            // 
+            // buttonMain
+            // 
+            this.buttonMain.Location = new System.Drawing.Point(637, 340);
+            this.buttonMain.Name = "buttonMain";
+            this.buttonMain.Size = new System.Drawing.Size(168, 40);
+            this.buttonMain.TabIndex = 57;
+            this.buttonMain.Text = "Заполнить";
+            this.buttonMain.UseVisualStyleBackColor = true;
+            this.buttonMain.Click += new System.EventHandler(this.ButtonMain_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(631, 39);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(140, 21);
+            this.buttonCancel.TabIndex = 58;
+            this.buttonCancel.Text = "Отменить заполнение";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
+            // 
+            // buttonHorizontal
+            // 
+            this.buttonHorizontal.Location = new System.Drawing.Point(6, 19);
+            this.buttonHorizontal.Name = "buttonHorizontal";
+            this.buttonHorizontal.Size = new System.Drawing.Size(168, 23);
+            this.buttonHorizontal.TabIndex = 59;
+            this.buttonHorizontal.Text = "Горизонтальное ребро";
+            this.buttonHorizontal.UseVisualStyleBackColor = true;
+            this.buttonHorizontal.Click += new System.EventHandler(this.ButtonHorizontal_Click);
+            // 
+            // buttonVertical
+            // 
+            this.buttonVertical.Location = new System.Drawing.Point(6, 48);
+            this.buttonVertical.Name = "buttonVertical";
+            this.buttonVertical.Size = new System.Drawing.Size(168, 23);
+            this.buttonVertical.TabIndex = 60;
+            this.buttonVertical.Text = "Вертикальное ребро";
+            this.buttonVertical.UseVisualStyleBackColor = true;
+            this.buttonVertical.Click += new System.EventHandler(this.ButtonVertical_Click);
+            // 
+            // groupBoxSpecific
+            // 
+            this.groupBoxSpecific.Controls.Add(this.buttonLastDelete);
+            this.groupBoxSpecific.Controls.Add(this.buttonVertical);
+            this.groupBoxSpecific.Controls.Add(this.buttonHorizontal);
+            this.groupBoxSpecific.Location = new System.Drawing.Point(631, 225);
+            this.groupBoxSpecific.Name = "groupBoxSpecific";
+            this.groupBoxSpecific.Size = new System.Drawing.Size(185, 109);
+            this.groupBoxSpecific.TabIndex = 61;
+            this.groupBoxSpecific.TabStop = false;
+            this.groupBoxSpecific.Text = "Помощь при вводе";
+            // 
+            // buttonLastDelete
+            // 
+            this.buttonLastDelete.Location = new System.Drawing.Point(6, 77);
+            this.buttonLastDelete.Name = "buttonLastDelete";
+            this.buttonLastDelete.Size = new System.Drawing.Size(168, 21);
+            this.buttonLastDelete.TabIndex = 61;
+            this.buttonLastDelete.Text = "Удалить последнюю вершину";
+            this.buttonLastDelete.UseVisualStyleBackColor = true;
+            this.buttonLastDelete.Click += new System.EventHandler(this.ButtonLastDelete_Click);
+            // 
+            // resultMessage
+            // 
+            this.resultMessage.AutoSize = true;
+            this.resultMessage.Location = new System.Drawing.Point(634, 473);
+            this.resultMessage.Name = "resultMessage";
+            this.resultMessage.Size = new System.Drawing.Size(75, 13);
+            this.resultMessage.TabIndex = 62;
+            this.resultMessage.Text = "resultMessage";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(643, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 63;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(738, 392);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 64;
+            this.label2.Text = "Y";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(663, 389);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(47, 20);
+            this.textBoxX.TabIndex = 65;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(758, 389);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(47, 20);
+            this.textBoxY.TabIndex = 66;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(637, 415);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(168, 40);
+            this.buttonEnter.TabIndex = 67;
+            this.buttonEnter.Text = "Ввести точку";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // radioButtonStringest
+            // 
+            this.radioButtonStringest.AutoSize = true;
+            this.radioButtonStringest.Location = new System.Drawing.Point(10, 62);
+            this.radioButtonStringest.Name = "radioButtonStringest";
+            this.radioButtonStringest.Size = new System.Drawing.Size(79, 17);
+            this.radioButtonStringest.TabIndex = 56;
+            this.radioButtonStringest.Text = "Построчно";
+            this.radioButtonStringest.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 641);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultMessage);
+            this.Controls.Add(this.groupBoxSpecific);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonMain);
+            this.Controls.Add(this.groupBoxWay);
+            this.Controls.Add(this.groupColor);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.coordsLabel);
+            this.Controls.Add(this.mainCanvas);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.Text = "Лабораторная работа 5. Хетагуров Павел";
+            ((System.ComponentModel.ISupportInitialize)(this.mainCanvas)).EndInit();
+            this.groupColor.ResumeLayout(false);
+            this.groupColor.PerformLayout();
+            this.groupBoxWay.ResumeLayout(false);
+            this.groupBoxWay.PerformLayout();
+            this.groupBoxSpecific.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainCanvas;
+        private System.Windows.Forms.Label coordsLabel;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.GroupBox groupColor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButtonWayQuick;
+        private System.Windows.Forms.RadioButton radioButtonWayStep;
+        private System.Windows.Forms.GroupBox groupBoxWay;
+        private System.Windows.Forms.Button buttonMain;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonHorizontal;
+        private System.Windows.Forms.Button buttonVertical;
+        private System.Windows.Forms.GroupBox groupBoxSpecific;
+        private System.Windows.Forms.Button buttonLastDelete;
+        private System.Windows.Forms.Label resultMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.RadioButton radioButtonStringest;
+    }
+}
+
---
+++ lab_05/sources/MainForm.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab_05
+{
+    public partial class MainForm : Form
+    {
+        Color workBackColor;
+
+        Graphics graph;
+        Bitmap workBitmap;
+
+        List<List<Point> > polygons = new List<List<Point>>();
+
+        List<Point> currentPolygon = new List<Point>();
+
+        int maxX = 0;
+
+        bool expectedVertical = false;
+        bool expectedHorizontal = false;
+
+        Agregator agregator = new Agregator();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            workBackColor = mainCanvas.BackColor;
+            workBitmap = new Bitmap(mainCanvas.Width, mainCanvas.Height);
+            graph = Graphics.FromImage(workBitmap);
+            mainCanvas.Image = workBitmap;
+        }
+
+        private void UpdateMainBitmap()
+        {
+            mainCanvas.Image = workBitmap;
+            mainCanvas.Update();
+        }
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            SetChooseColor((Button)sender);
+        }
+
+        private void SetChooseColor(Button colorButton)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.Color = colorButton.BackColor;
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+                colorButton.BackColor = colorDialog.Color;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.Color = workBackColor;
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                graph.Clear(colorDialog.Color);
+                workBackColor = colorDialog.Color;
+            }
+            maxX = 0;
+            polygons.Clear();
+            currentPolygon.Clear();
+            UpdateMainBitmap();
+        }
+
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            Redraw();
+
+        }
+
+        private void Redraw()
+        {
+            graph.Clear(workBackColor);
+            agregator = new Agregator();
+            Pen pen = new Pen(colorButton.BackColor);
+            foreach (var polygon in polygons)
+            {
+                graph.DrawPolygon(pen, polygon.ToArray());
+            }
+
+            for (int i = 0; i < currentPolygon.Count - 1; i++)
+            {
+                graph.DrawLine(pen, currentPolygon[i], currentPolygon[i + 1]);
+            }
+            UpdateMainBitmap();
+        }
+
+        private void ButtonLastDelete_Click(object sender, EventArgs e)
+        {
+            if (currentPolygon.Count > 0)
+            {
+                currentPolygon.RemoveAt(currentPolygon.Count - 1);
+            }
+            else if (polygons.Count > 0)
+            {
+                currentPolygon = polygons[polygons.Count - 1];
+                polygons.RemoveAt(polygons.Count - 1);
+                currentPolygon.RemoveAt(currentPolygon.Count - 1);
+            }
+
+            Redraw();
+        }
+
+        private void ButtonHorizontal_Click(object sender, EventArgs e)
+        {
+            expectedHorizontal = true;
+            expectedVertical = false;
+        }
+
+        private void ButtonVertical_Click(object sender, EventArgs e)
+        {
+            expectedVertical = true;
+            expectedHorizontal = false;
+        }
+
+        private void ButtonMain_Click(object sender, EventArgs e)
+        {
+            if (polygons.Count >= 1 && currentPolygon.Count == 0)
+            {
+                if (radioButtonWayQuick.Checked)
+                {
+                    if (!agregator.isEmpty)
+                    {
+                        Redraw();
+                    }
+                    var startTime = System.Diagnostics.Stopwatch.StartNew();
+                    UpdateMainBitmap();
+                    workBitmap.SetPixel(1, 1, Color.Black);
+                    for (int i = 0; i < 1; i++)
+                    {
+                        Agregator.FillPolygon(ExtractRibs(polygons), maxX, colorButton.BackColor, workBackColor, workBitmap);
+                    }
+
+                    startTime.Stop();
+                    var resultMillisecondsTime = startTime.Elapsed.TotalMilliseconds / 100;
+                    resultMessage.Text = String.Format("Результат: {0}ms", resultMillisecondsTime.ToString());
+
+                    UpdateByBitmap(workBitmap);
+                }
+                else if (radioButtonStringest.Checked)
+                {
+                    if (agregator.isEmpty)
+                    {
+                        agregator = new Agregator(ExtractRibs(polygons), maxX, colorButton.BackColor, workBackColor, workBitmap);
+                    }
+                    if (agregator.NextStep());
+                    {
+                        Agregator.DrawRibs(workBitmap, ExtractRibs(polygons), colorButton.BackColor);
+                    }
+                    UpdateByBitmap(agregator.workBitmap);
+                }
+                else if (radioButtonWayStep.Checked)
+                {
+                    agregator = new Agregator(ExtractRibs(polygons), maxX, colorButton.BackColor, workBackColor, workBitmap);
+                    while (!agregator.NextStep())
+                    {
+                        UpdateByBitmap(agregator.workBitmap);
+                        Thread.Sleep(10);
+                    }
+                    Agregator.DrawRibs(workBitmap, ExtractRibs(polygons), colorButton.BackColor);
+                    UpdateByBitmap(agregator.workBitmap);
+                }
+            }
+            else
+            {
+                if (polygons.Count == 0)
+                {
+                    MessageBox.Show("Введите хотя бы один многоугольник");
+                }
+                if (currentPolygon.Count != 0)
+                {
+                    MessageBox.Show("Текущий вводимый многоугольник незамкнут");
+                }
+            }
+        }
+
+        private List<(Point, Point)> ExtractRibs(List<List<Point> > polygons)
+        {
+            List<(Point, Point)> ribs = new List<(Point, Point)>();
+
+            foreach(var polygon in polygons)
+            {
+                for (int i = 0; i < polygon.Count - 1; i++)
+                {
+                    ribs.Add((polygon[i], polygon[i + 1]));
+                }
+                ribs.Add((polygon[0], polygon[polygon.Count - 1]));
+            }
+
+            return ribs;
+        }
+
+        private void UpdateByBitmap(Bitmap bitmap)
+        {
+            UpdateMainBitmap();
+        }
+
+        private void mainCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point point = e.Location;
+            if (expectedHorizontal)
+            {
+                if (currentPolygon.Count > 0)
+                {
+                    point.Y = currentPolygon[currentPolygon.Count - 1].Y;
+                    expectedHorizontal = false;
+                }
+            }
+            else if (expectedVertical)
+            {
+                if (currentPolygon.Count > 0)
+                {
+                    point.X = currentPolygon[currentPolygon.Count - 1].X;
+                    expectedVertical = false;
+                }
+            }
+            EnterDot(point);
+        }
+
+        private void EnterDot(Point point)
+        {
+            maxX = Math.Max(maxX, point.X);
+            if (currentPolygon.Contains(point))
+            {
+                polygons.Add(currentPolygon);
+                currentPolygon = new List<Point>();
+            }
+            else
+            {
+                currentPolygon.Add(point);
+            }
+
+            Redraw();
+        }
+        private void mainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            coordsLabel.Text = e.Location.ToString();
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            Point curPoint = new Point();
+            curPoint.X = int.Parse(textBoxX.Text);
+            curPoint.Y = int.Parse(textBoxY.Text);
+            EnterDot(curPoint);
+        }
+    }
+
+}
