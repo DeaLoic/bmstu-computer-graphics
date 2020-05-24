77c5cd6938d7b4fd248113f0e9fca3b23732011a
Pavel Khetagurov <khetagurow1@yandex.ru>
2020-05-24 23:28:34 +0300

    [lab_09] /f"

---
+++ lab_09/sources/Agregator.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Security;
+
+namespace lab_09
+{
+
+    internal static class Agregator
+    {
+        public static List<Point> Cutting(List<Point> cutter, List<Point> polygon)
+        {
+
+            if (!IsConvex(cutter))
+            {
+                throw new Exception("Отсекатель не выпуклый");
+            }
+            else
+            {
+                List<Vector> normalVectors = FormNormalVectors(cutter);
+                List<Point> newTempPolygon;
+
+                Vector D;
+                Vector W;
+
+                double scalarD;
+                double scalarW;
+                double t;
+
+                Point tempPoint;
+                for (int i = 0; i < cutter.Count; i++)
+                {
+                    newTempPolygon = new List<Point>();
+                    if (new Vector(cutter[i], GetVertex(polygon, 0)).ScalarMultiplication(normalVectors[i]) > 0)
+                    {
+                        newTempPolygon.Add(GetVertex(polygon, 0));
+                    }
+
+                    for (int j = 0; j < polygon.Count; j++)
+                    {
+                        D = new Vector(GetVertex(polygon, j), GetVertex(polygon, j + 1));
+                        scalarD = D.ScalarMultiplication(normalVectors[i]);
+
+                        if (scalarD != 0)
+                        {
+
+                            W = new Vector(cutter[i], polygon[j]);
+                            scalarW = W.ScalarMultiplication(normalVectors[i]);
+                            t = -scalarW / scalarD;
+
+                            if (0 <= t && t <= 1)
+                            {
+                                newTempPolygon.Add(CutByParam(t, (GetVertex(polygon, j), GetVertex(polygon, j + 1))));
+                            }
+                        }
+
+                        if (new Vector(cutter[i], GetVertex(polygon, j + 1)).ScalarMultiplication(normalVectors[i]) > 0)
+                        {
+                            newTempPolygon.Add(GetVertex(polygon, j + 1));
+                        }
+                    }
+
+                    polygon = newTempPolygon;
+                }
+            }
+
+            return polygon;
+        }
+
+        private static bool IsConvex(List<Point> cutter)
+        {
+            int sign = 0;
+            bool isConvex = true;
+
+            if (cutter.Count >= 3)
+            {
+                Vector first = new Vector(cutter[0], cutter[1]);
+                Vector second;
+                Vector result;
+
+                for (int i = 0; i < cutter.Count && isConvex; i++)
+                {
+                    second = new Vector(GetVertex(cutter, i), GetVertex(cutter, i + 1));
+                    result = first.VectorMultiplication(second);
+
+                    if (sign == 0)
+                    {
+                        sign = Math.Sign(result.Z);
+                    }
+                    else if ((sign != Math.Sign(result.Z)) && (result.Z != 0))
+                    {
+                        sign = 0;
+                        isConvex = false;
+                    }
+
+                    first = second;
+                }
+            }
+
+            if (sign == 0)
+            {
+                isConvex = false;
+            }
+
+            return isConvex;
+        }
+
+        private static List<Vector> FormNormalVectors(List<Point> cutter)
+        {
+            List<Vector> normalVectors = new List<Vector>();
+            Vector vector;
+            Vector result;
+
+            for (int i = 0; i < cutter.Count; i++)
+            {
+                vector = new Vector(GetVertex(cutter, i), GetVertex(cutter, i + 1));
+                if (vector.X != 0)
+                {
+                    result = new Vector(-vector.Y / vector.X, 1);
+                }
+                else
+                {
+                    result = new Vector(1, -vector.X / vector.Y);
+                }
+
+                if (result.ScalarMultiplication(new Vector(GetVertex(cutter, i - 1), GetVertex(cutter, i + 1))) > 0)
+                {
+                    result.X = -result.X;
+                    result.Y = -result.Y;
+                }
+
+                normalVectors.Add(result);
+            }
+
+            return normalVectors;
+        }
+        private static (Point, Point) CutByParam(double t_down, double t_up, (Point, Point) line)
+        {
+            return (new Point((int)Math.Round(line.Item1.X + (line.Item2.X - line.Item1.X) * t_down),
+                              (int)Math.Round(line.Item1.Y + (line.Item2.Y - line.Item1.Y) * t_down)),
+
+                    new Point((int)Math.Round(line.Item1.X + (line.Item2.X - line.Item1.X) * t_up),
+                              (int)Math.Round(line.Item1.Y + (line.Item2.Y - line.Item1.Y) * t_up)));
+        }
+
+        private static Point CutByParam(double t, (Point, Point) line)
+        {
+            return new Point((int)Math.Round(line.Item1.X + (line.Item2.X - line.Item1.X) * t),
+                              (int)Math.Round(line.Item1.Y + (line.Item2.Y - line.Item1.Y) * t));
+        }
+
+        private static Point GetVertex(List<Point> cutter, int index)
+        {
+            if (index >= 0)
+            {
+                return cutter[index % cutter.Count];
+            }
+            else
+            {
+                return cutter[cutter.Count - 1 + index % cutter.Count];
+            }
+        }
+    }
+}
---
+++ lab_09/sources/MainForm.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace lab_09
+{
+    public partial class MainForm : Form
+    {
+        Color workBackColor;
+        Pen cutterPen = new Pen(Color.Red);
+        Pen linesPen = new Pen(Color.DarkBlue);
+
+        Graphics graph;
+        Bitmap workBitmap;
+
+        List<Point> cutter = new List<Point>();
+        List<Point> polygon = new List<Point>();
+
+        bool expectedVertical = false;
+        bool expectedHorizontal = false;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+            workBackColor = mainCanvas.BackColor;
+            workBitmap = new Bitmap(mainCanvas.Width, mainCanvas.Height);
+            graph = Graphics.FromImage(workBitmap);
+            mainCanvas.Image = workBitmap;
+        }
+
+        private void UpdateMainBitmap()
+        {
+            mainCanvas.Image = workBitmap;
+            mainCanvas.Update();
+        }
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            SetChooseColor((Button)sender);
+        }
+
+        private void SetChooseColor(Button colorButton)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.Color = colorButton.BackColor;
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+                colorButton.BackColor = colorDialog.Color;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            colorDialog.Color = workBackColor;
+
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                graph.Clear(colorDialog.Color);
+                workBackColor = colorDialog.Color;
+            }
+            cutter.Clear();
+            polygon.Clear();
+            UpdateMainBitmap();
+        }
+
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            Redraw();
+        }
+
+        private void Redraw()
+        {
+            graph.Clear(workBackColor);
+
+            if (cutter.Count > 2)
+            {
+                graph.DrawPolygon(cutterPen, cutter.ToArray());
+            }
+            else if (cutter.Count == 2)
+            {
+                graph.DrawLine(cutterPen, cutter[0], cutter[1]);
+            }
+
+            if (polygon.Count > 2)
+            {
+                graph.DrawPolygon(linesPen, polygon.ToArray());
+            }
+            else if (polygon.Count == 2)
+            {
+                graph.DrawLine(cutterPen, polygon[0], polygon[1]);
+            }
+
+            UpdateMainBitmap();
+        }
+
+        private void ButtonLastDelete_Click(object sender, EventArgs e)
+        {
+            if (polygon.Count > 0)
+            {
+                polygon.RemoveAt(polygon.Count - 1);
+            }
+
+            Redraw();
+        }
+
+        private void ButtonHorizontal_Click(object sender, EventArgs e)
+        {
+            expectedHorizontal = true;
+            expectedVertical = false;
+        }
+
+        private void ButtonVertical_Click(object sender, EventArgs e)
+        {
+            expectedVertical = true;
+            expectedHorizontal = false;
+        }
+
+        private void MainFillLogic()
+        {
+            if (cutter.Count >= 3 && polygon.Count >= 3)
+            {
+                UpdateMainBitmap();
+                try
+                {
+                    var resPolygon = Agregator.Cutting(cutter, polygon);
+
+                    graph.DrawPolygon(new Pen(colorButton.BackColor), resPolygon.ToArray());
+
+                    UpdateByBitmap();
+
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите отсекатель и хотя бы один отрезок");
+            }
+
+        }
+
+        private void UpdateByBitmap()
+        {
+            UpdateMainBitmap();
+        }
+
+        private void mainCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point point = e.Location;
+            Console.WriteLine(e.Button);
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (expectedHorizontal)
+                {
+                    if (radioButtonEnterLine.Checked && polygon.Count > 0)
+                    {
+                        point.Y = polygon[polygon.Count - 1].Y;
+                        expectedHorizontal = false;
+                    }
+                    else if (radioButtonEnterRect.Checked && cutter.Count > 0)
+                    {
+                        point.Y = cutter[cutter.Count - 1].Y;
+                        expectedHorizontal = false;
+                    }
+                }
+                else if (expectedVertical)
+                {
+                    if (radioButtonEnterLine.Checked && polygon.Count > 0)
+                    {
+                        point.X = polygon[polygon.Count - 1].X;
+                        expectedVertical = false;
+                    }
+                    else if (radioButtonEnterRect.Checked && cutter.Count > 0)
+                    {
+                        point.X = cutter[cutter.Count - 1].X;
+                        expectedVertical = false;
+                    }
+                }
+                EnterDot(point);
+            }
+            else
+            {
+                MainFillLogic();
+            }
+        }
+
+        private void EnterDot(Point point)
+        {
+            if (radioButtonEnterLine.Checked)
+            {
+                polygon.Add(point);
+            }
+            else
+            {
+                if (cutter.Count > 0 && cutter[0] == point)
+                {
+                    MessageBox.Show("Отсекатель уже введен");
+                }
+                else
+                {
+                    cutter.Add(point);
+                }
+            }
+
+            Redraw();
+        }
+        private void mainCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            coordsLabel.Text = e.Location.ToString();
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            Point curPoint = Point.Empty;
+            curPoint.X = int.Parse(textBoxX1.Text);
+            curPoint.Y = int.Parse(textBoxY1.Text);
+            EnterDot(curPoint);
+        }
+    }
+
+}
